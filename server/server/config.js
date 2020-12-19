d2b0b4ad0615d9df4eb45fdfed8e68726191bff9
yu274151 <yu274151@users.noreply.github.com>
2020-12-19 01:59:35 -0500

    Corrected password value as a string

--- server/server/config.js
+++ server/server/config.js
@@ -1,3 +1,3 @@
 module.exports = { db: 'mongodb+srv://jihan:Jcriki04@cluster0.m2xf1.mongodb.net/<dbname>retryWrites=true&w=majority', 
-                   pwd: test1234
+                   pwd: 'test1234'
                  }
